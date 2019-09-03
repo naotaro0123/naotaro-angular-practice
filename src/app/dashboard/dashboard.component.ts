26462c2056d1491788725dcac8f1678bc91c202f
naotaro0123 <n.ikewada@gmail.com>
2019-09-03 12:27:12 +0900

    fix compile error

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HeroService } from "../hero.service";
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,14 +10,15 @@ import { HeroService } from "../hero.service";
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService
+      .getHeroes()
+      .subscribe(heroes => (this.heroes = heroes.slice(1, 5)));
   }
 }
